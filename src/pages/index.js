53fe678b92a02d2367318f5b93b7a0042203af2b
ilya <ilya@belsky.in>
2020-12-08 20:26:36 +0100

    Hardcode today's video

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,13 @@ import { Helmet } from "react-helmet"
 const IndexPage = () => (
   <>
     <Helmet title="Today's yoga" />
-    <div>YWA Now</div>
+    <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/lMTyp5npt78"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+    allowfullscreen></iframe>
   </>
 )
 
