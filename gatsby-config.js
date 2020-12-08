60c50475997f9d05f2a85818cb2a3a0df62cb557
ilya <ilya@belsky.in>
2020-12-08 20:13:59 +0100

    Remove gatsby's default page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Yoga With Adriene NOW`,
+    description: `Get to today's yoga right now`,
+    author: `@oluckyman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
