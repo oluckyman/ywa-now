4748f43fbf46009c8ef14c67ac9b670f687003c4
Ilya Belsky <elias.logins@gmail.com>
2021-10-31 19:09:24 +0100

    Redirect to today's video from Novemter calendar

--- pages/api/today.ts
+++ pages/api/today.ts
@@ -1,6 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { promises as fs } from 'fs'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.redirect(`https://www.youtube.com/playlist?list=WL`)
+interface Video {
+  id: string
+  day: number
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const [, month, day] = new Date().toISOString().substr(0, 10).split(`-`)
+  const json: Video[] = await fs
+    .readFile(`calendars/${month}.json`, `utf8`)
+    .then((txt) => JSON.parse(txt))
+    .catch((e) => {
+      res.send(JSON.stringify(e))
+    })
+  const videos = json.filter((d) => d.id).filter((d) => d.day === +day)
+  if (videos.length === 1) {
+    res.redirect(`https://www.youtube.com/watch?v=${videos[0].id}`)
+  } else {
+    res.json(videos)
+  }
 }
