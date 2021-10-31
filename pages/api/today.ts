55f945bd11ef1e1f9ff8a634cc198d137a36c4d8
Ilya Belsky <elias.logins@gmail.com>
2021-10-31 19:15:47 +0100

    Handle missing json error

--- pages/api/today.ts
+++ pages/api/today.ts
@@ -8,13 +8,14 @@ interface Video {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const [, month, day] = new Date().toISOString().substr(0, 10).split(`-`)
-  const json: Video[] = await fs
+  const videos: Video[] | void = await fs
     .readFile(`calendars/${month}.json`, `utf8`)
-    .then((txt) => JSON.parse(txt))
+    .then((txt) => JSON.parse(txt) as Video[])
+    .then((json) => json.filter((d) => d.id).filter((d) => d.day === +day))
     .catch((e) => {
-      res.send(JSON.stringify(e))
+      res.json(e)
     })
-  const videos = json.filter((d) => d.id).filter((d) => d.day === +day)
+  if (!videos) return
   if (videos.length === 1) {
     res.redirect(`https://www.youtube.com/watch?v=${videos[0].id}`)
   } else {
